4c0cf6de17634c4c4d4fb166b1a2eaec137c53d2
Tim Nolet <timnolet@hotmail.com>
2020-07-20 22:26:17 +0200

    feat: WIP: super simple landing

--- blog/.vuepress/theme/components/Home.vue
+++ blog/.vuepress/theme/components/Home.vue
@@ -4,17 +4,10 @@
     aria-labelledby="main-title"
   >
     <header class="hero">
-      <img
-        v-if="data.heroImage"
-        :src="$withBase(data.heroImage)"
-        :alt="data.heroAlt || 'hero'"
-      >
-
       <h1
-        v-if="data.heroText !== null"
         id="main-title"
       >
-        {{ data.heroText || $title || 'Hello' }}
+        👩‍💻 Learn Puppeteer & Playwright
       </h1>
 
       <p class="description">
@@ -46,7 +39,19 @@
       </div>
     </div>
 
-    <Content class="theme-default-content custom" />
+    <div class="theme-default-content custom">
+      <h3>Recent activity</h3>
+      <ul>
+        <li
+          v-for="page in recentPosts"
+          :key="page.path"
+        >
+          <router-link :to="page.path">
+            {{ page.title }}
+          </router-link>
+        </li>
+      </ul>
+    </div>
 
     <div
       class="footer"
@@ -59,7 +64,6 @@
 
 <script>
 import NavLink from './NavLink.vue'
-
 export default {
   components: { NavLink },
 
@@ -73,6 +77,14 @@ export default {
         link: this.data.actionLink,
         text: this.data.actionText
       }
+    },
+
+    recentPosts () {
+      const posts = this.$site.pages
+        .filter(page => {
+          return page.id === 'post'
+        })
+      return posts.slice(0, 10)
     }
   }
 }
@@ -81,11 +93,11 @@ export default {
 <style lang="stylus">
 .home
   padding $navbarHeight 2rem 0
-  max-width 960px
-  margin 0px auto
+  max-width $contentWidth
+  margin 0 auto
   display block
   .hero
-    text-align center
+    text-align left
     img
       max-width: 100%
       max-height 280px
@@ -94,12 +106,12 @@ export default {
     h1
       font-size 3rem
     h1, .description, .action
-      margin 1.8rem auto
+      margin 1.8rem 0
     .description
-      max-width 35rem
+      max-width $contentWidth
       font-size 1.6rem
       line-height 1.3
-      color lighten($textColor, 40%)
+      color $darkTextColor
     .action-button
       display inline-block
       font-size 1.2rem
