e7c9e1d0fcf86cae02e7acdf9051515dfb2f24f7
Giovanni Rago <ragog90@gmail.com>
2020-07-31 11:10:51 +0200

    feat: basic browser start and navigation

---
+++ blog/snippets/puppeteer/basic-browser-navigation.js
@@ -0,0 +1,8 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://danube-store.herokuapp.com')
+  await browser.close()
+})()
