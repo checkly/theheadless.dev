908a2b3e97eca962901fab80e88f0da1119e69e1
Maxi Gimenez <maxi@checklyhq.com>
2020-08-19 18:54:36 +0200

    chore: add more tags

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -117,6 +117,8 @@ module.exports = {
     ['link', { rel: 'shortcut icon', href: '/favicon.png' }],
     ['meta', { property: 'og:image', content: shareImage }],
     ['meta', { name: 'twitter:image', content: shareImage }],
+    ['meta', { name: 'twitter:title', content: 'theheadless.dev - Learn Puppeteer & Playwright' }],
+    ['meta', { name: 'twitter:url', content: 'https://theheadless.dev' }],
     ['meta', { name: 'twitter:card', content: 'summary_large_image' }]
   ]
 }
