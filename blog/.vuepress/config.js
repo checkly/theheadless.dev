3191ac17603e2445f883d420acdf9507eba5693c
Hannes Lenke <hlenke@testobject.org>
2020-11-05 07:29:00 +0100

    fix comma.

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -122,8 +122,8 @@ module.exports = {
     ['meta', { name: 'twitter:image', content: shareImage }],
     ['meta', { name: 'twitter:title', content: 'theheadless.dev - Learn Puppeteer & Playwright' }],
     ['meta', { name: 'twitter:url', content: 'https://theheadless.dev' }],
-    ['meta', { name: 'twitter:card', content: 'summary_large_image' }]
-    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }]
   ],
   patterns: [
     '**/*.md',
