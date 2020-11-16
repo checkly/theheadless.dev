1fb77aa1138adf6038d77852dffa2d69cba0219a
ndom91 <yo@ndo.dev>
2020-11-16 19:31:49 +0100

    style: adjust img min-width

--- blog/.vuepress/theme/components/Footer.vue
+++ blog/.vuepress/theme/components/Footer.vue
@@ -176,7 +176,7 @@ export default {
     letter-spacing: 0.4px;
     color: #FF4949;
     font-weight: bold;
-    min-width: 350px;
+    min-width: 270px;
     text-align: right;
     margin-right: 40px;
   }
